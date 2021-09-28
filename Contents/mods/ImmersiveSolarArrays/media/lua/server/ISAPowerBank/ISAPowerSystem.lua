998b276a57a9590cfde4a0c71565c44270ec8550
radx5Blue <91193589+radx5Blue@users.noreply.github.com>
2021-09-28 12:05:35 +0800

    Update ISAPowerSystem.lua
    reloading and leaving and re-entering cell/grid work now

--- Contents/mods/ImmersiveSolarArrays/media/lua/server/ISAPowerBank/ISAPowerSystem.lua
+++ Contents/mods/ImmersiveSolarArrays/media/lua/server/ISAPowerBank/ISAPowerSystem.lua
@@ -22,6 +22,7 @@ function TurnOnPower(powerConsumption, numberOfPanels, square, createKey)
             local pbX = ModData.get("PBX")
             local pbY = ModData.get("PBY")
             local pbZ = ModData.get("PBZ")
+			local pbNP = ModData.get("PBNP")
 
             local pbkLen = #pbKey
             local newpbKLen = pbkLen + 1
@@ -30,6 +31,7 @@ function TurnOnPower(powerConsumption, numberOfPanels, square, createKey)
             table.insert(pbX, newpbKLen, square:getX())
             table.insert(pbY, newpbKLen, square:getY())
             table.insert(pbZ, newpbKLen, square:getZ())
+			table.insert(pbNP, newpbKLen, numberOfPanels)
 
             sqX = square:getX()
             sqY = square:getY()
@@ -68,6 +70,7 @@ function PowerCheck()
     testX = ModData.get("PBX")
     testY = ModData.get("PBY")
     testZ = ModData.get("PBZ")
+	testNP = ModData.get("PBNP")
 
     local pbkLen = #testK
 
@@ -81,10 +84,34 @@ function PowerCheck()
         noX = tonumber(testX[key])
         noY = tonumber(testY[key])
         noZ = tonumber(testZ[key])
+		noPZ = tonumber(testNP[key])
 
         local square = getWorld():getCell():getGridSquare(noX, noY, noZ)
 
-        solarscan(square, false, true, false, 0)
+		if square ~= nil then
+			
+		player = getPlayer()
+
+        local NewGenerator = IsoGenerator.new(nil, player:getCell(), square)
+        NewGenerator:setConnected(false)
+        NewGenerator:setFuel(0)
+        NewGenerator:setCondition(0)
+        NewGenerator:setActivated(false)
+        NewGenerator:setSurroundingElectricity()
+        NewGenerator:remove()
+
+        local NewGenerator = IsoGenerator.new(nil, player:getCell(), square)
+        NewGenerator:setConnected(true)
+        NewGenerator:setFuel(100)
+        NewGenerator:setCondition(100)
+        NewGenerator:setActivated(true)
+        NewGenerator:setSurroundingElectricity()
+        NewGenerator:remove()
+        print("Solar Array Re-created")
+			
+			
+		end
+	
 		
     end
 
@@ -181,11 +208,13 @@ function SetUpGlobalData()
     local powerBankX = {}
     local powerBankY = {}
     local powerBankZ = {}
+	local NumberOfPanels = {}
 
     ModData.add("PBK", powerBankKey)
     ModData.add("PBX", powerBankX)
     ModData.add("PBY", powerBankY)
     ModData.add("PBZ", powerBankZ)
+	ModData.add("PBNP", NumberOfPanels)
 end
 
 Events.OnNewGame.Add(SetUpGlobalData)
