e38c22978331a40fa842da127109a748ee7bd51d
kattiko <45799955+kattiko@users.noreply.github.com>
2021-09-28 13:31:03 +0300

    randomness to battery degradation

--- Contents/mods/ImmersiveSolarArrays/media/lua/client/PowerBank/ISAInventoryHandler.lua
+++ Contents/mods/ImmersiveSolarArrays/media/lua/client/PowerBank/ISAInventoryHandler.lua
@@ -29,19 +29,19 @@ for i=1,container:getItems():size() do
 local item = container:getItems():get(i-1)
 local type = item:getType()
 			if type == "50AhBattery" then
-			item:setCondition(item:getCondition() - 5)
+			item:setCondition(item:getCondition() - ZombRand(9)+1)
 			--breaks in 20 days
 			end
 			if type == "75AhBattery" then
-			item:setCondition(item:getCondition() - 4)
+			item:setCondition(item:getCondition() - ZombRand(8)+1)
 			--breaks in 25 days
 			end
 			if type == "100AhBattery" then
-			item:setCondition(item:getCondition() - 3)
+			item:setCondition(item:getCondition() - ZombRand(6)+1)
 			--breaks in 33 days 
 			end
 			if type == "DeepCycleBattery" then
-			item:setCondition(item:getCondition() - 0.03)
+			item:setCondition(item:getCondition() - (ZombRand(59)+1) / 1000 )
 			--breaks in 9+ years
 			end
 end
