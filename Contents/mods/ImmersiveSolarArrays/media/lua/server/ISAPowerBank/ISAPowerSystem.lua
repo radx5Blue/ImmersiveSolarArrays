a3d2ffe5b5f2adb0f3ba783a9883a83fa0a1e6b7
radx5Blue <91193589+radx5Blue@users.noreply.github.com>
2021-09-29 19:37:45 +0800

    Update ISAPowerSystem.lua

--- Contents/mods/ImmersiveSolarArrays/media/lua/server/ISAPowerBank/ISAPowerSystem.lua
+++ Contents/mods/ImmersiveSolarArrays/media/lua/server/ISAPowerBank/ISAPowerSystem.lua
@@ -22,8 +22,8 @@ function TurnOnPower(powerConsumption, numberOfPanels, square, createKey)
             local pbX = ModData.get("PBX")
             local pbY = ModData.get("PBY")
             local pbZ = ModData.get("PBZ")
-			local pbNP = ModData.get("PBNP")
-			local pbLD = ModData.get("PBLD")
+            local pbNP = ModData.get("PBNP")
+            local pbLD = ModData.get("PBLD")
 
             local pbkLen = #pbKey
             local newpbKLen = pbkLen + 1
@@ -32,8 +32,8 @@ function TurnOnPower(powerConsumption, numberOfPanels, square, createKey)
             table.insert(pbX, newpbKLen, square:getX())
             table.insert(pbY, newpbKLen, square:getY())
             table.insert(pbZ, newpbKLen, square:getZ())
-			table.insert(pbNP, newpbKLen, numberOfPanels)
-			table.insert(pbLD, newpbKLen, "1")
+            table.insert(pbNP, newpbKLen, numberOfPanels)
+            table.insert(pbLD, newpbKLen, "1")
 
             sqX = square:getX()
             sqY = square:getY()
@@ -42,7 +42,7 @@ function TurnOnPower(powerConsumption, numberOfPanels, square, createKey)
             print("Created Passed X: ", sqX)
             print("Created Passed Y: ", sqY)
             print("Created Passed Z: ", sqZ)
-        end		
+        end
 
         local NewGenerator = IsoGenerator.new(nil, square:getCell(), square)
         NewGenerator:setConnected(false)
@@ -63,7 +63,6 @@ function TurnOnPower(powerConsumption, numberOfPanels, square, createKey)
     end
 end
 
-
 function DisconnectPower(square)
     sqX = square:getX()
     sqY = square:getY()
@@ -73,13 +72,12 @@ function DisconnectPower(square)
     testX = ModData.get("PBX")
     testY = ModData.get("PBY")
     testZ = ModData.get("PBZ")
-	testNP = ModData.get("PBNP")
-	testL = ModData.get("PBLD")
+    testNP = ModData.get("PBNP")
+    testL = ModData.get("PBLD")
 
     local pbkLen = #testK
 
     for key = 1, #testK do
-
         noKey = tonumber(testK[key])
         noX = tonumber(testX[key])
         noY = tonumber(testY[key])
@@ -91,7 +89,6 @@ function DisconnectPower(square)
 
         if (sqX == noX and sqY == noY and sqZ == noZ) then
             squareTest = getWorld():getCell():getGridSquare(sqX, sqY, sqZ)
-
 
             local NewGenerator = IsoGenerator.new(nil, square:getCell(), squareTest)
             NewGenerator:setFuel(0)
@@ -105,184 +102,136 @@ function DisconnectPower(square)
             table.remove(testX, key, sqX)
             table.remove(testY, key, sqY)
             table.remove(testZ, key, sqZ)
-			table.remove(testNP, key, testNP)
-			table.remove(testL, key, testL)
-
+            table.remove(testNP, key, testNP)
+            table.remove(testL, key, testL)
         end
     end
-
 end
-
 
 function CheckGlobalData()
     local powerBankKey = {}
     local powerBankX = {}
     local powerBankY = {}
     local powerBankZ = {}
-	local NumberOfPanels = {}
-	local PowerBankLoaded = {}
-	
-	if ModData.exists("PBK") == false then
-		
-		ModData.add("PBK", powerBankKey)
-		ModData.add("PBX", powerBankX)
-		ModData.add("PBY", powerBankY)
-		ModData.add("PBZ", powerBankZ)
-		ModData.add("PBNP", NumberOfPanels)
-		ModData.add("PBLD", PowerBankLoaded)
-		
-	end
-		
+    local NumberOfPanels = {}
+    local PowerBankLoaded = {}
+
+    if ModData.exists("PBK") == false then
+        ModData.add("PBK", powerBankKey)
+        ModData.add("PBX", powerBankX)
+        ModData.add("PBY", powerBankY)
+        ModData.add("PBZ", powerBankZ)
+        ModData.add("PBNP", NumberOfPanels)
+        ModData.add("PBLD", PowerBankLoaded)
+    end
 end
 
 function getModifiedSolarOutput(SolarInput)
-	local myWeather = getClimateManager()
-	local cloudiness = 1 - (myWeather:getCloudiness() * 0.25)
-	local fogginess = 1 - (myWeather:getFogStrength() * 0.25)
-	local currentHour = getGameTime():getHour()
-	local output = SolarInput * 83
-	output = output * cloudiness
-	output = output * fogginess
-		if currentHour < myWeather:getDawn() or currentHour > myWeather:getDusk() then
-			--it's night, no power
-			output = 0
-		end
-return output
+    local myWeather = getClimateManager()
+    local cloudiness = 1 - (myWeather:getCloudiness() * 0.25)
+    local fogginess = 1 - (myWeather:getFogStrength() * 0.25)
+    local currentHour = getGameTime():getHour()
+    local output = SolarInput * 83
+    output = output * cloudiness
+    output = output * fogginess
+    if currentHour < myWeather:getDawn() or currentHour > myWeather:getDusk() then
+        --it's night, no power
+        output = 0
+    end
+    return output
 end
 
 local function ReloadPower()
-	
     local testK = ModData.get("PBK")
     local testX = ModData.get("PBX")
     local testY = ModData.get("PBY")
     local testZ = ModData.get("PBZ")
-	local testNP = ModData.get("PBNP")
-	local testL = ModData.get("PBLD")
+    local testNP = ModData.get("PBNP")
+    local testL = ModData.get("PBLD")
 
     local pbkLen = #testK
-	
 
     for key = 1, #testK do
         print("Check ModData Key: ", testK[key])
         print("Check ModData X: ", testX[key])
         print("Check ModData Y: ", testY[key])
         print("Check ModData Z: ", testZ[key])
-		print("Check ModData NP: ", testNP[key])
-		print("Check ModData LOADED: ", testL[key])
+        print("Check ModData NP: ", testNP[key])
+        print("Check ModData LOADED: ", testL[key])
 
         noKey = tonumber(testK[key])
         noX = tonumber(testX[key])
         noY = tonumber(testY[key])
         noZ = tonumber(testZ[key])
-		noPZ = tonumber(testNP[key])
-		noLD = tonumber(testL[key])
+        noPZ = tonumber(testNP[key])
+        noLD = tonumber(testL[key])
 
-		if (getWorld():getCell():getGridSquare(noX, noY, noZ) ~= nil) then
-			
-		local square = getWorld():getCell():getGridSquare(noX, noY, noZ)
-		
-		local NewGenerator = IsoGenerator.new(nil, square:getCell(), square)
-        NewGenerator:setConnected(false)
-        NewGenerator:setFuel(0)
-        NewGenerator:setCondition(0)
-        NewGenerator:setActivated(false)
-        NewGenerator:setSurroundingElectricity()
-        NewGenerator:remove()
-		
-		
-        local NewGenerator = IsoGenerator.new(nil, square:getCell(), square)
-        NewGenerator:setConnected(true)
-        NewGenerator:setFuel(100)
-        NewGenerator:setCondition(100)
-        NewGenerator:setActivated(true)
-        NewGenerator:setSurroundingElectricity()
-        NewGenerator:remove()
-		testL[key] = "0"
-		
-        print("Solar Array Re-created")
-		--print("Solar Array Re-created and: ", testL[key])
-		
-			
-		end
-	
-		
+        if (getWorld():getCell():getGridSquare(noX, noY, noZ) ~= nil) then
+            local square = getWorld():getCell():getGridSquare(noX, noY, noZ)
+
+            local NewGenerator = IsoGenerator.new(nil, square:getCell(), square)
+            NewGenerator:setConnected(false)
+            NewGenerator:setFuel(0)
+            NewGenerator:setCondition(0)
+            NewGenerator:setActivated(false)
+            NewGenerator:setSurroundingElectricity()
+            NewGenerator:remove()
+
+            local NewGenerator = IsoGenerator.new(nil, square:getCell(), square)
+            NewGenerator:setConnected(true)
+            NewGenerator:setFuel(100)
+            NewGenerator:setCondition(100)
+            NewGenerator:setActivated(true)
+            NewGenerator:setSurroundingElectricity()
+            NewGenerator:remove()
+            testL[key] = "0"
+
+            print("Solar Array Re-created")
+        --print("Solar Array Re-created and: ", testL[key])
+        end
     end
-
-	
-
 end
 
 function PowerCheck()
-	
     local testK = ModData.get("PBK")
     local testX = ModData.get("PBX")
     local testY = ModData.get("PBY")
     local testZ = ModData.get("PBZ")
-	local testNP = ModData.get("PBNP")
-	local testL = ModData.get("PBLD")
-	
-	
-	
-	for key = 1, #testK do
-		
-			 noKey = tonumber(testK[key])
+    local testNP = ModData.get("PBNP")
+    local testL = ModData.get("PBLD")
+
+    for key = 1, #testK do
+        noKey = tonumber(testK[key])
         noX = tonumber(testX[key])
         noY = tonumber(testY[key])
         noZ = tonumber(testZ[key])
-		noPZ = tonumber(testNP[key])
-		noLD = tonumber(testL[key])
-		
-		
-		--local player = getPlayer()
-		
-			local square = getWorld():getCell():getGridSquare(noX, noY, noZ)
-		
-		if (testL[key] == "1" and square ~= nil) then
-			testL[key] = "0"
-			print("Test KEY in IF: ", testL[key])
-			
-			
-		local NewGenerator = IsoGenerator.new(nil, square:getCell(), square)
-        NewGenerator:setConnected(false)
-        NewGenerator:setFuel(0)
-        NewGenerator:setCondition(0)
-        NewGenerator:setActivated(false)
-        NewGenerator:setSurroundingElectricity()
-        NewGenerator:remove()
-		
-		local NewGenerator = IsoGenerator.new(nil, square:getCell(), square)
-        NewGenerator:setConnected(true)
-        NewGenerator:setFuel(100)
-        NewGenerator:setCondition(100)
-       NewGenerator:setActivated(true)
-       NewGenerator:setSurroundingElectricity()
-		print("gen b4 remove: ", NewGenerator)
-		NewGenerator:remove()
-	   --ReloadPower()
-		print("gen after remove: ", NewGenerator)
-		testL[key] = "0"
-		
-			
-		elseif (testL[key] == "0" and square == nil) then
-			testL[key] = "1"
-			local pbl = ModData.get("PBLD")
-			print("Test KEY in ELSE: ", pbl[key])
-			
-		end
-		
-		
-		
-		
-			
-	end
-	
+        noPZ = tonumber(testNP[key])
+        noLD = tonumber(testL[key])
 
+        local square = getWorld():getCell():getGridSquare(noX, noY, noZ)
+
+        if (square ~= nil) then
+            local NewGenerator = IsoGenerator.new(nil, square:getCell(), square)
+            NewGenerator:setConnected(false)
+            NewGenerator:setFuel(0)
+            NewGenerator:setCondition(0)
+            NewGenerator:setActivated(false)
+            NewGenerator:setSurroundingElectricity()
+            NewGenerator:remove()
+
+            local NewGenerator = IsoGenerator.new(nil, square:getCell(), square)
+            NewGenerator:setConnected(true)
+            NewGenerator:setFuel(100)
+            NewGenerator:setCondition(100)
+            NewGenerator:setActivated(true)
+            NewGenerator:remove()
+        end
+    end
 end
 
-
-
 --Events.OnNewGame.Add(SetUpGlobalData)
-Events.EveryTenMinutes.Add(PowerCheck)
---Events.OnTick.Add(PowerCheck)
+--Events.EveryTenMinutes.Add(PowerCheck)
+--Events.OnPostMapLoad.Add(PowerCheck)
+Events.OnTick.Add(PowerCheck)
 Events.OnGameStart.Add(CheckGlobalData)
 Events.OnGameStart.Add(ReloadPower)
