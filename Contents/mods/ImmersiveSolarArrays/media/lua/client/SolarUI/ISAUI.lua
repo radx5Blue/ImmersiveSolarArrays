1a687e99f2adbae40db70f66476ee5ff893ef00d
radx5Blue <91193589+radx5Blue@users.noreply.github.com>
2021-10-16 20:30:50 +0800

    menu updates

--- Contents/mods/ImmersiveSolarArrays/media/lua/client/SolarUI/ISAUI.lua
+++ Contents/mods/ImmersiveSolarArrays/media/lua/client/SolarUI/ISAUI.lua
@@ -1,16 +1,88 @@
-ISAMenu = ISAMenu or {};
-ISAMenu._index = ISAMenu
+-- ISAMenu = ISAMenu or {};
+-- ISAMenu._index = ISAMenu
 
-ISAMenu.createMenuEntries = function(_player, _context, _worldObjects)
+-- ISAMenu.createMenuEntries = function(_player, _context, _worldObjects)
 
-	local context = _context;
-	local worldobjects = _worldObjects; 
+	-- local context = _context;
+	-- local worldobjects = _worldObjects; 
 	
 	
 
 	
 	
 	
+	-- -- local testK = ModData.get("PBK")
+    -- -- local testX = ModData.get("PBX")
+    -- -- local testY = ModData.get("PBY")
+    -- -- local testZ = ModData.get("PBZ")
+    -- -- local testNP = ModData.get("PBNP")
+    -- -- local testL = ModData.get("PBLD")
+    -- -- local testC = ModData.get("PBCH")
+    -- -- local testB = ModData.get("PBBO")
+    -- -- local testG = ModData.get("PBGN")
+
+    -- -- player = getPlayer()
+
+    -- -- for key = 1, #testK do
+        -- -- noKey = tonumber(testK[key])
+        -- -- noX = tonumber(testX[key])
+        -- -- noY = tonumber(testY[key])
+        -- -- noZ = tonumber(testZ[key])
+        -- -- noPZ = tonumber(testNP[key])
+        -- -- noLD = tonumber(testL[key])
+        -- -- noCH = tonumber(testC[key])
+        -- -- noPB = tonumber(testB[key])
+        -- -- noGN = tonumber(testG[key])
+
+		
+		
+	 -- -- subMenu:addOption("Battery Bank", worldobjects, function() SaySolar() end);
+	 
+ -- -- end
+ 
+ 	-- if test and ISWorldObjectContextMenu.Test then return true end
+
+	-- local bank = nil
+
+	-- local objects = {}
+	-- for _,object in ipairs(worldobjects) do
+		-- local square = object:getSquare()
+		-- if square then
+			-- for i=1,square:getObjects():size() do
+				-- local object2 = square:getObjects():get(i-1)
+				-- if ISMoveableSpriteProps:findOnSquare(square, "solarmod_tileset_01_0") then
+					-- bank = object2
+				-- end
+			-- end
+		-- end
+	-- end
+
+	-- if not bank then return end
+	
+	-- if bank ~= nil then
+		
+			-- local ISAOption = context:addOption("Battery Bank", worldobjects, function() OpenBatterBankInfo(square) end);
+	-- --local subMenu = ISContextMenu:getNew(context);
+	-- --context:addSubMenu(ISAOption, subMenu);
+	
+	-- ISAOption.context = context
+	-- ISAOption.subMenu = subMenu
+		
+	-- end
+	 
+
+
+	
+
+-- end
+
+
+-- OpenBatterBankInfo = function(square)
+
+    -- sqX = square:getX()
+    -- sqY = square:getY()
+    -- sqZ = square:getZ()
+
 	-- local testK = ModData.get("PBK")
     -- local testX = ModData.get("PBX")
     -- local testY = ModData.get("PBY")
@@ -33,92 +105,20 @@ ISAMenu.createMenuEntries = function(_player, _context, _worldObjects)
         -- noCH = tonumber(testC[key])
         -- noPB = tonumber(testB[key])
         -- noGN = tonumber(testG[key])
+		
+
+        -- if (sqX == noX and sqY == noY and sqZ == noZ) then
+         
+		 -- -- do logic in here
+		 
+	 -- end
 
 		
-		
-	 -- subMenu:addOption("Battery Bank", worldobjects, function() SaySolar() end);
 	 
  -- end
- 
- 	if test and ISWorldObjectContextMenu.Test then return true end
-
-	local bank = nil
-
-	local objects = {}
-	for _,object in ipairs(worldobjects) do
-		local square = object:getSquare()
-		if square then
-			for i=1,square:getObjects():size() do
-				local object2 = square:getObjects():get(i-1)
-				if ISMoveableSpriteProps:findOnSquare(square, "solarmod_tileset_01_0") then
-					bank = object2
-				end
-			end
-		end
-	end
-
-	if not bank then return end
-	
-	if bank ~= nil then
-		
-			local ISAOption = context:addOption("Battery Bank", worldobjects, function() OpenBatterBankInfo(square) end);
-	--local subMenu = ISContextMenu:getNew(context);
-	--context:addSubMenu(ISAOption, subMenu);
-	
-	ISAOption.context = context
-	ISAOption.subMenu = subMenu
-		
-	end
-	 
 
 
-	
-
-end
+-- end
 
 
-OpenBatterBankInfo = function(square)
-
-    sqX = square:getX()
-    sqY = square:getY()
-    sqZ = square:getZ()
-
-	local testK = ModData.get("PBK")
-    local testX = ModData.get("PBX")
-    local testY = ModData.get("PBY")
-    local testZ = ModData.get("PBZ")
-    local testNP = ModData.get("PBNP")
-    local testL = ModData.get("PBLD")
-    local testC = ModData.get("PBCH")
-    local testB = ModData.get("PBBO")
-    local testG = ModData.get("PBGN")
-
-    player = getPlayer()
-
-    for key = 1, #testK do
-        noKey = tonumber(testK[key])
-        noX = tonumber(testX[key])
-        noY = tonumber(testY[key])
-        noZ = tonumber(testZ[key])
-        noPZ = tonumber(testNP[key])
-        noLD = tonumber(testL[key])
-        noCH = tonumber(testC[key])
-        noPB = tonumber(testB[key])
-        noGN = tonumber(testG[key])
-		
-
-        if (sqX == noX and sqY == noY and sqZ == noZ) then
-         
-		 -- do logic in here
-		 
-	 end
-
-		
-	 
- end
-
-
-end
-
-
-Events.OnFillWorldObjectContextMenu.Add(ISAMenu.createMenuEntries);
+-- Events.OnFillWorldObjectContextMenu.Add(ISAMenu.createMenuEntries);
