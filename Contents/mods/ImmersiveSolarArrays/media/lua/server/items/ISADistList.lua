dda1bad15dbf31ff1e2cf52b6440501fd1402df8
radx5Blue <91193589+radx5Blue@users.noreply.github.com>
2021-10-17 22:48:55 +0800

    Update ISADistList.lua

--- Contents/mods/ImmersiveSolarArrays/media/lua/server/items/ISADistList.lua
+++ Contents/mods/ImmersiveSolarArrays/media/lua/server/items/ISADistList.lua
@@ -22,10 +22,10 @@ registerAsLoot(iReg, 0.6, "ShelfGeneric");
 -- Solar panel
 
 iReg = "ISA.SolarPanel";
-registerAsLoot(iReg, 0.08, "CrateCamping");
+registerAsLoot(iReg, 0.10, "CrateCamping");
 registerAsLoot(iReg, 0.10, "CrateElectronics");
-registerAsLoot(iReg, 0.08, "CrateMechanics");
-registerAsLoot(iReg, 0.08, "ToolStoreMetalwork");
+registerAsLoot(iReg, 0.10, "CrateMechanics");
+registerAsLoot(iReg, 0.10, "ToolStoreMetalwork");
 registerAsLoot(iReg, 0.10, "ToolStoreTools");
 registerAsLoot(iReg, 0.15, "ToolStoreMisc");
 registerAsLoot(iReg, 0.20, "StoreShelfElectronics");
@@ -36,17 +36,17 @@ registerAsLoot(iReg, 0.10, "CrateRandomJunk");
 registerAsLoot(iReg, 0.10, "CrateTools");
 registerAsLoot(iReg, 0.10, "CampingStoreGear");
 registerAsLoot(iReg, 0.10, "OtherGeneric");
-registerAsLoot(iReg, 0.08, "GarageMechanic");
-registerAsLoot(iReg, 0.08, "ElectronicStoreAppliances");
-registerAsLoot(iReg, 0.08, "ToolStoreFarming");
+registerAsLoot(iReg, 0.10, "GarageMechanic");
+registerAsLoot(iReg, 0.10, "ElectronicStoreAppliances");
+registerAsLoot(iReg, 0.10, "ToolStoreFarming");
 
 
 -- Deep cycle battery
 
 iReg = "ISA.DeepCycleBattery";
-registerAsLoot(iReg, 0.06, "JanitorMisc");
+registerAsLoot(iReg, 0.10, "JanitorMisc");
 registerAsLoot(iReg, 0.10, "StoreShelfElectronics");
-registerAsLoot(iReg, 0.04, "MechanicShelfElectric");
+registerAsLoot(iReg, 0.10, "MechanicShelfElectric");
 registerAsLoot(iReg, 0.15, "StoreShelfMechanics");
 registerAsLoot(iReg, 0.10, "CrateElectronics");
 registerAsLoot(iReg, 0.10, "CrateMechanics");
@@ -55,31 +55,31 @@ registerAsLoot(iReg, 0.15, "ToolStoreMisc");
 registerAsLoot(iReg, 0.10, "ArmyStorageElectronics");
 registerAsLoot(iReg, 0.10, "ElectronicStoreMisc");
 registerAsLoot(iReg, 0.10, "CrateRandomJunk");
-registerAsLoot(iReg, 0.08, "CrateTools");
-registerAsLoot(iReg, 0.08, "OtherGeneric");
-registerAsLoot(iReg, 0.08, "GarageMechanic");
-registerAsLoot(iReg, 0.08, "ToolStoreFarming");
+registerAsLoot(iReg, 0.10, "CrateTools");
+registerAsLoot(iReg, 0.10, "OtherGeneric");
+registerAsLoot(iReg, 0.10, "GarageMechanic");
+registerAsLoot(iReg, 0.10, "ToolStoreFarming");
 
 
 
 -- Inverter
 
 iReg = "ISA.ISAInverter";
-registerAsLoot(iReg, 0.05, "StoreShelfElectronics");
-registerAsLoot(iReg, 0.07, "StoreShelfMechanics");
-registerAsLoot(iReg, 0.05, "CrateElectronics");
-registerAsLoot(iReg, 0.07, "CrateMechanics");
-registerAsLoot(iReg, 0.07, "MechanicShelfMisc");
-registerAsLoot(iReg, 0.07, "MechanicShelfElectric");
+registerAsLoot(iReg, 0.10, "StoreShelfElectronics");
+registerAsLoot(iReg, 0.10, "StoreShelfMechanics");
+registerAsLoot(iReg, 0.10, "CrateElectronics");
+registerAsLoot(iReg, 0.10, "CrateMechanics");
+registerAsLoot(iReg, 0.10, "MechanicShelfMisc");
+registerAsLoot(iReg, 0.10, "MechanicShelfElectric");
 registerAsLoot(iReg, 0.10, "ToolStoreMisc");
 registerAsLoot(iReg, 0.10, "ToolStoreTools");
-registerAsLoot(iReg, 0.05, "GigamartHouseElectronics");
-registerAsLoot(iReg, 0.05, "ArmyStorageElectronics");
-registerAsLoot(iReg, 0.05, "ElectronicStoreMisc");
-registerAsLoot(iReg, 0.02, "CrateRandomJunk");
+registerAsLoot(iReg, 0.10, "GigamartHouseElectronics");
+registerAsLoot(iReg, 0.10, "ArmyStorageElectronics");
+registerAsLoot(iReg, 0.10, "ElectronicStoreMisc");
+registerAsLoot(iReg, 0.10, "CrateRandomJunk");
 registerAsLoot(iReg, 0.10, "CrateTools");
-registerAsLoot(iReg, 0.20, "OtherGeneric");
-registerAsLoot(iReg, 0.05, "GarageMechanic");
-registerAsLoot(iReg, 0.05, "ElectronicStoreAppliances");
+registerAsLoot(iReg, 0.10, "OtherGeneric");
+registerAsLoot(iReg, 0.10, "GarageMechanic");
+registerAsLoot(iReg, 0.10, "ElectronicStoreAppliances");
 registerAsLoot(iReg, 0.10, "ToolStoreFarming");
 
