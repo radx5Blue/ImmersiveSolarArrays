271161f2bb64814783e5f047bcf6c29e227dbddf
radx5Blue <91193589+radx5Blue@users.noreply.github.com>
2021-10-18 11:16:13 +0800

    Update ISAWordSpawns.lua

--- Contents/mods/ImmersiveSolarArrays/media/lua/server/items/ISAWordSpawns.lua
+++ Contents/mods/ImmersiveSolarArrays/media/lua/server/items/ISAWordSpawns.lua
@@ -1,17 +1,29 @@
-
-test = false
-
 function SpawnPanels()
 	
 	 local player = getPlayer()
+	 
+	local pKeys = ModData.get("SpawnCellKeys")
+    local pX = ModData.get("SpawnCellX")
+    local pY = ModData.get("SpawnCellY")
+    local pZ = ModData.get("SpawnCellZ")
+    local pType = ModData.get("SpawnCellType")
 	
-	local square = getWorld():getCell():getGridSquare(4723, 7998, 0)
+
+    local keyIndex = #pKeys
+
+    for key = 1, #pKeys do
+		
+        noKey = tonumber(pKeys[key])
+        noX = tonumber(pX[key])
+        noY = tonumber(pY[key])
+        noZ = tonumber(pZ[key])
+
+	local square = getWorld():getCell():getGridSquare(noX, noY, noZ)
 	
-	
-	
+	if square ~= nil then
 	if (calculateDistance(player:getX(), player:getY(), square:getX(), square:getY()) < 5 and test == false) then
 		
-	local sprite_type = tostring("solarmod_tileset_01_7")
+	local sprite_type = tostring(pType[key])
     if not sprite_type then
         print("NO SPRITE TYPE!")
         return false
@@ -28,18 +40,23 @@ function SpawnPanels()
     end
     square:getObjects():add(newSprite)
     square:RecalcProperties()    
+	
+	player:Say("Solars")
+	
+	table.remove(pKeys, key, key)
+    table.remove(pX, key, noX)
+    table.remove(pY, key, noY)
+    table.remove(pZ, key, noZ)
+    table.remove(pType, key, pType)
+     end
 		
 		
 
-		test = true
-		
-		
-		
-		
-		
-		
-		
+	
+	
 	end
+	
+end
 	
 	
 	
@@ -50,5 +67,51 @@ function SpawnPanels()
 end
 
 
+function SpawnRolls()
+	
+	local spawnCellKeys = {}
+	local spawnCellX = {}
+	local spawnCellY = {}
+	local spawnCellZ = {}
+	local spawnCellType = {}
+	--local spawnCellChance = {}
+	--local spawnCellHasSpawned = {}
+
+
+    if ModData.exists("SpawnCellKeys") == false then
+
+		
+		local CellKeysNumber = 0
+		
+		table.insert(spawnCellKeys, CellKeysNumber + 1, CellKeysNumber + 1)
+		table.insert(spawnCellX, CellKeysNumber, 4723)
+		table.insert(spawnCellY, CellKeysNumber, 7997)
+		table.insert(spawnCellZ, CellKeysNumber, 0)
+		table.insert(spawnCellType, CellKeysNumber, "solarmod_tileset_01_7")
+		
+		
+		table.insert(spawnCellKeys, CellKeysNumber + 1, CellKeysNumber + 1)
+		table.insert(spawnCellX, CellKeysNumber, 4744)
+		table.insert(spawnCellY, CellKeysNumber, 7849)
+		table.insert(spawnCellZ, CellKeysNumber, 0)
+		table.insert(spawnCellType, CellKeysNumber, "solarmod_tileset_01_7")
+		
+		
+		ModData.add("SpawnCellKeys", spawnCellKeys)
+		ModData.add("SpawnCellX", spawnCellX)
+		ModData.add("SpawnCellY", spawnCellY)
+		ModData.add("SpawnCellZ", spawnCellZ)
+		ModData.add("SpawnCellType", spawnCellType)
+		--ModData.add("SpawnCellChance", spawnCellChance)
+		--ModData.add("SpawnCellHasSpawned", spawnCellHasSpawned)
+		
+	end
+
+	
+	
+	
+end
+
 
 Events.OnPlayerUpdate.Add(SpawnPanels)
+Events.OnGameStart.Add(SpawnRolls)
