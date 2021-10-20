23f399ebf8b42485a4399dedc1fa50eb350a6b07
kattiko <45799955+kattiko@users.noreply.github.com>
2021-10-20 19:41:54 +0300

    changed options

--- Contents/mods/ImmersiveSolarArrays/media/lua/client/ISASandboxOptions.lua
+++ Contents/mods/ImmersiveSolarArrays/media/lua/client/ISASandboxOptions.lua
@@ -3,4 +3,7 @@ print( "CustomOption2=", SandboxVars.ISA.CustomOption2 )
 print( "CustomOption3=", SandboxVars.ISA.CustomOption3 )
 print( "CustomOption4=", SandboxVars.ISA.CustomOption4 )
 print( "CustomOption5=", SandboxVars.ISA.CustomOption5 )
-print( "CustomOption5=", SandboxVars.ISA.CustomOption6 )
+print( "CustomOption6=", SandboxVars.ISA.CustomOption6 )
+print( "CustomOption7=", SandboxVars.ISA.CustomOption7 )
+print( "CustomOption8=", SandboxVars.ISA.CustomOption8 )
+
