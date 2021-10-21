bf46eb9b3ee40cefb3d3a6d7ff9aa6bf2917d5ad
radx5Blue <91193589+radx5Blue@users.noreply.github.com>
2021-10-21 10:19:57 +0800

    Sandbox Values passing to the game

--- Contents/mods/ImmersiveSolarArrays/media/lua/client/ISASandboxOptions.lua
+++ Contents/mods/ImmersiveSolarArrays/media/lua/client/ISASandboxOptions.lua
@@ -1,4 +1,10 @@
-print( "CustomOption1=", SandboxVars.ISA.CustomOption1 )
+SandboxVars = require "Sandbox/Survival"
+
+
+getSandboxOptions():initSandboxVars()
+
+
+print( "Solar Panel Efficiency =", SandboxVars.ISA.solarPanelEfficiency )
 print( "CustomOption2=", SandboxVars.ISA.CustomOption2 )
 print( "CustomOption3=", SandboxVars.ISA.CustomOption3 )
 print( "CustomOption4=", SandboxVars.ISA.CustomOption4 )
@@ -6,4 +12,3 @@ print( "CustomOption5=", SandboxVars.ISA.CustomOption5 )
 print( "CustomOption6=", SandboxVars.ISA.CustomOption6 )
 print( "CustomOption7=", SandboxVars.ISA.CustomOption7 )
 print( "CustomOption8=", SandboxVars.ISA.CustomOption8 )
-
--- Contents/mods/ImmersiveSolarArrays/media/lua/server/ISAPowerBank/ISAPowerSystem.lua
+++ Contents/mods/ImmersiveSolarArrays/media/lua/server/ISAPowerBank/ISAPowerSystem.lua
@@ -1,6 +1,6 @@
 require 'PowerBank/ISAInventoryHandler'
 
-local ISASolarEfficiency = 25;
+local ISASolarEfficiency = SandboxVars.ISA.solarPanelEfficiency;
 
 function TurnOnPower(powerConsumption, numberOfPanels, square, createKey)
     print("numberOfPanels: ", numberOfPanels * ISASolarEfficiency)
@@ -34,7 +34,7 @@ function TurnOnPower(powerConsumption, numberOfPanels, square, createKey)
         sqY = square:getY()
         sqZ = square:getZ()
 
-        if numberOfPanels * (83 * ((ISASolarEfficiency * 1.2) / 100)  > powerConsumption then 
+        if numberOfPanels * (83 * ((ISASolarEfficiency * 1.2) / 100))  > powerConsumption then 
             local NewGenerator = IsoGenerator.new(nil, square:getCell(), square)
             NewGenerator:setConnected(false)
             NewGenerator:setFuel(0)
@@ -511,7 +511,7 @@ function getModifiedSolarOutput(SolarInput)
     local light = getClimateManager():getDayLightStrength()
     local fogginess = getClimateManager():getFogIntensity()
     local CloudinessFogginessMean = 1 - (((cloudiness + fogginess) / 2) * 0.25) --make it so that clouds and fog can only reduce output by 25%
-    local output = SolarInput * (83 * ((ISASolarEfficiency * 1.2) / 100) --changed to more realistic 1993 levels
+    local output = SolarInput * (83 * ((ISASolarEfficiency * 1.2) / 100)) --changed to more realistic 1993 levels
 	local temperature = getClimateManager():getTemperature()
 	local temperaturefactor = temperature * -0.0035 + 1.1 --based on linear single crystal sp efficiency
     output = output * CloudinessFogginessMean
@@ -936,6 +936,9 @@ function GenCheck()
             end
         end
     end
+	
+	
+	print("SHOW ME**************************************************************************************************************", ISASolarEfficiency)
 	
 	
 end
