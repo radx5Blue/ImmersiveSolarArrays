4f05107e5c975c49cc2c879b2ca9553a90ac829a
Poltergeistzx <igor.mov0@gmail.com>
2023-09-06 23:16:51 +0300

    remove debug test function

--- Contents/mods/ImmersiveSolarArrays/media/lua/server/World/ISAMapObjects.lua
+++ Contents/mods/ImmersiveSolarArrays/media/lua/server/World/ISAMapObjects.lua
@@ -37,22 +37,3 @@ if not isClient then
         MapObjects.OnNewWithSprite(sprite, OnNewWithSprite, 5)
     end
 end
-
---- Debug
---todo remove
-local prev_AcceptItemFunction = "AcceptItemFunction.ISA_Batteries"
-local function debugAcceptItemFunction(index)
-    index = index or 1
-    if not isa.PbSystem_client or isa.PbSystem_client:getLuaObjectCount() < index then return end
-    local pb = isa.PbSystem_client:getLuaObjectByIndex(index)
-    local isoObject = pb and pb:getIsoObject()
-    if not isoObject then return end
-    local AcceptItemFunction = isoObject:getContainer():getAcceptItemFunction()
-    if AcceptItemFunction ~= prev_AcceptItemFunction then
-        local player = getPlayer()
-        if player then player:Say("ISA debug AcceptItemFunction changed") end
-        print(string.format("ISA debug AcceptItemFunction changed: %.1f ", math.floor(getGameTime():getWorldAgeHours())), AcceptItemFunction ~= nil)
-        prev_AcceptItemFunction = AcceptItemFunction
-    end
-end
-Events.EveryTenMinutes.Add(debugAcceptItemFunction)
